481b5a21f815f004b3355a31a900400383f5b669
Fred Ma <fred.ma@blackrock.com>
2025-10-05 20:15:36 -0400

    Adds User and UserService classes Modifies InvoiceService to use UserService to check user exists Modifies Application to inject UserService into InvoiceService

--- src/main/java/com/marcobehler/myfancypdfinvoices/services/InvoiceService.java
+++ src/main/java/com/marcobehler/myfancypdfinvoices/services/InvoiceService.java
@@ -1,12 +1,19 @@
 package com.marcobehler.myfancypdfinvoices.services;
 
 import com.marcobehler.myfancypdfinvoices.model.Invoice;
+import com.marcobehler.myfancypdfinvoices.model.User;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class InvoiceService {
 
+    private UserService userService;
+
     private List<Invoice> invoices = new CopyOnWriteArrayList<>(); //
+
+    public InvoiceService(UserService userService) {
+        this.userService = userService;
+    }
 
     public List<Invoice> findAll() {
         return invoices;
@@ -14,6 +21,11 @@ public class InvoiceService {
 
     public Invoice create(String userId, Integer amount) {
         // TODO real pdf creation and storing it on network server
+        User user = userService.findById(userId);
+        if (user == null) {
+            throw new IllegalStateException();
+        }
+
         Invoice invoice = new Invoice(userId, amount, "http://www.africau.edu/images/default/sample.pdf");
         invoices.add(invoice);
         return invoice;
